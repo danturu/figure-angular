72de6bcfd26e4002aebb862619dffa776e9cd346
Dimitri Rosenberg <di@radiant.fm>
2015-11-15 14:55:58 +0300

    add directive that assign local vars

---
+++ app/client/directives/assign_local.ts
@@ -0,0 +1,24 @@
+import { Directive, ViewRef, ViewContainerRef, TemplateRef} from 'angular2/angular2'
+
+@Directive({
+  selector: '[assign-local]', inputs: ['local: assignLocalTo']
+})
+
+export class AssignLocal {
+  view: ViewRef;
+
+  constructor(private viewContainer: ViewContainerRef, private templateRef: TemplateRef) {
+  }
+
+  set local(expression) {
+     if (!this.viewContainer.length) {
+       this.view = this.viewContainer.createEmbeddedView(this.templateRef);
+     } else {
+       this.view = this.viewContainer.get(0);
+     }
+
+    this.view.setLocal("$implicit", expression);
+  }
+}
+
+
