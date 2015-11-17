ee3bb3aa226b7978b304106bc9597c2fbd5899ee
Dimitri Rosenberg <di@radiant.fm>
2015-11-17 22:05:30 +0300

    add universal base exception

---
+++ lib/base_exception.ts
@@ -0,0 +1,12 @@
+export abstract class BaseException extends Error {
+  public stack: any;
+
+  constructor(public message: string = "--") {
+    super(message);
+    this.stack = (<any>new Error(message)).stack;
+  }
+
+  toString(): string {
+    return this.message;
+  }
+}
